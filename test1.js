3a8fdb96d80423457273055ae46bde3db40d547a
mo <48237066+KingBoyAndGirl@users.noreply.github.com>
2023-09-28 17:36:09 +0800

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,25 @@
+/*
+MOLDIV 解锁会员
+
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https://graph\.facebook\.com/v\d+\.\d+/\d+/activities url script-response-body https://raw.githubusercontent.com/KingBoyAndGirl/test/main/test1.js
+
+[mitm]
+hostname = graph.facebook.com
+**************************/
+
+var url = $request.url;
+var body = $response.body;
+console.log("1============================:" + body);
+if (url.indexOf("/activities") != -1) {
+    var modifiedData = {
+        "success": true
+    };
+
+    body = JSON.stringify(modifiedData);
+}
+console.log("2============================:" + body);
+$done({ body });
