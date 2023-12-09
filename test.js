4e448b5561fadf16ea809d021bef5e06e12e503c
mo <48237066+KingBoyAndGirl@users.noreply.github.com>
2023-12-09 09:59:13 +0800

    Update test.js

--- test.js
+++ test.js
@@ -39,6 +39,30 @@ if (url.indexOf("https://planitphoto.b4a.io/classes/Purchase") != -1) {
                 },
                 "updatedAt": "2099-12-10T00:03:11.583Z",
                 "createdAt": "2099-12-10T00:03:11.583Z"
+            },
+            {
+              "objectId" : "kJAYaDkWPI",
+              "userID" : "7Y9LXJIPdg",
+              "order" : {
+                "updatedAt" : "2023-12-09T01:50:02.188Z",
+                "original_order_no" : "20231209015000909093",
+                "payment_method" : "Wechat",
+                "amount" : "4.99",
+                "__type" : "Object",
+                "order_date" : "2023-12-09 01:50:01",
+                "fee" : "0.11",
+                "sku" : "explorer_five_year",
+                "type" : "P",
+                "className" : "Sale",
+                "app" : "Planit China",
+                "objectId" : "9ry1TNPPXG",
+                "order_no" : "20231209015000909093",
+                "currency" : "USD",
+                "device_model" : "HUAWEI NOP-AN00",
+                "createdAt" : "2023-12-09T01:50:02.188Z"
+              },
+              "updatedAt" : "2023-12-09T01:55:46.988Z",
+              "createdAt" : "2023-12-09T01:55:46.988Z"
             }
         ]
     };
