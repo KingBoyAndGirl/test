4edf90d6a312af8b48984b79ce6b8d690af1a339
mo <48237066+KingBoyAndGirl@users.noreply.github.com>
2023-12-09 10:44:20 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,34 +1,98 @@
 /*
-MOLDIV 解锁会员
 
 ***************************
 QuantumultX:
 
 [rewrite_local]
-^https://firebaseinstallations\.googleapis\.com/v1/projects/jellybus-apps/installations/* url script-response-body https://raw.githubusercontent.com/KingBoyAndGirl/test/main/test2.js
+^https://planitphoto\.b4a\.io/classes/Purchase$ url script-response-body https://raw.githubusercontent.com/KingBoyAndGirl/test/main/qs.js
 [mitm]
-hostname = firebaseinstallations.googleapis.com
-**************************/
+hostname = planitphoto.b4a.io
+***************************/
 
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+    }
+
+    return result;
+}
+
+function generateOrderNo() {
+    const now = new Date();
+    now.setDate(now.getDate() - 1); // 减去一天
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+
+    let orderNo = `${year}${month}${day}${hours}${minutes}${seconds}${milliseconds}`;
+
+    // 如果当前orderNo的长度小于17，添加随机数字
+    while (orderNo.length < 17) {
+        orderNo += Math.floor(Math.random() * 10);
+    }
+
+    return {
+        orderNo,
+        updatedAt: `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`,
+        createdAt: `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`,
+        orderDate: `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+    };
+}
 
 var url = $request.url;
 var body = $response.body;
- console.log("1============================:" + body); 
-if (url.indexOf("https://firebaseinstallations.googleapis.com/v1/projects/jellybus-apps/installations/") != -1) {
-    var modifiedData = {
-        {
-  "refreshToken" : "3_AS3qfwJmGhPk5ATRGQ98mV8h-w-_jQ7l-LkNzfZKFOr6SDxFL_7qlgvq6cDFxYhyN3ejh8o0qfXGcmF2-J8ywpVMy-KTCGy87xbKF-H3kt_5u7w",
-  "fid" : "ceW0voFb0Uoqsl5He_mDqi",
-  "name" : "projects/932065416488/installations/ceW0voFb0Uoqsl5He_mDqi",
-  "authToken" : {
-    "token" : "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjE6OTMyMDY1NDE2NDg4Omlvczo1MDI5MmQ2MzVkZGE4ZGY1ZGYyYzFkIiwiZXhwIjoxNjk2NDk1NTgwLCJmaWQiOiJjZVcwdm9GYjBVb3FzbDVIZV9tRHFpIiwicHJvamVjdE51bWJlciI6OTMyMDY1NDE2NDg4fQ.AB2LPV8wRAIgKgxJCuqWrs7hdFK4AoA1ozTtoksAsR63VxI6DGqKPcgCIFZ7AumG3QJIHoftENKL_3i3dVYSSw8BLatwcEaIXKnh",
-    "expiresIn" : "604800s"
-  }
-}
 
+if (url.indexOf("https://planitphoto.b4a.io/classes/Purchase") != -1) {
+    var modifiedData = {
+        "results": [
+            {
+                "objectId": generateRandomString(10),
+                "userID": generateRandomString(10),
+                "order": {
+                    ...generateOrderNo(),
+                    "original_order_no": generateOrderNo().orderNo, // 修改这一行
+                    "payment_method": "Wechat",
+                    "amount": "19.99",
+                    "__type": "Object",
+                    "fee": "0.44",
+                    "sku": "model_five_year",
+                    "type": "P",
+                    "className": "Sale",
+                    "app": "Planit China",
+                    "device_model": "HUAWEI NOP-AN00",
+                }
+            },
+            {
+                "objectId": generateRandomString(10),
+                "userID": generateRandomString(10),
+                "order": {
+                    ...generateOrderNo(),
+                    "original_order_no": generateOrderNo().orderNo, // 修改这一行
+                    "payment_method": "Wechat",
+                    "amount": "4.99",
+                    "__type": "Object",
+                    "fee": "0.11",
+                    "sku": "explorer_five_year",
+                    "type": "P",
+                    "className": "Sale",
+                    "app": "Planit China",
+                    "device_model": "HUAWEI NOP-AN00",
+                }
+            }
+        ]
     };
-    
+
     body = JSON.stringify(modifiedData);
 }
- console.log("2============================:" + body); 
+
+console.log("2============================:" + body);
 $done({ body });
