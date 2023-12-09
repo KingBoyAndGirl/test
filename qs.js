76716823c83931902de975c45bce59169dbcaf94
mo <48237066+KingBoyAndGirl@users.noreply.github.com>
2023-12-09 10:12:45 +0800

    Update qs.js

--- qs.js
+++ qs.js
@@ -16,8 +16,8 @@ if (url.indexOf("https://planitphoto.b4a.io/classes/Purchase") != -1) {
     var modifiedData = {
         "results": [
             {
-                "objectId": "sDupKySZxx",
-                "userID": "7Y9LXJIPdm",
+                "objectId": "sDupKySZxd",
+                "userID": "7Y9LXJIPdg",
                 "order": {
                     "updatedAt": "2099-12-10T00:01:48.949Z",
                     "original_order_no": "209912100001478322",
@@ -43,12 +43,12 @@ if (url.indexOf("https://planitphoto.b4a.io/classes/Purchase") != -1) {
               "objectId" : "kJAYaDkWPI",
               "userID" : "7Y9LXJIPdg",
               "order" : {
-                "updatedAt" : "2023-12-09T01:50:02.188Z",
-                "original_order_no" : "20231209015000909093",
+                "updatedAt" : "2099-12-09T01:50:02.188Z",
+                "original_order_no" : "20991209015000909093",
                 "payment_method" : "Wechat",
                 "amount" : "4.99",
                 "__type" : "Object",
-                "order_date" : "2023-12-09 01:50:01",
+                "order_date" : "2099-12-09 01:50:01",
                 "fee" : "0.11",
                 "sku" : "explorer_five_year",
                 "type" : "P",
@@ -58,10 +58,10 @@ if (url.indexOf("https://planitphoto.b4a.io/classes/Purchase") != -1) {
                 "order_no" : "20991209015000909093",
                 "currency" : "USD",
                 "device_model" : "HUAWEI NOP-AN00",
-                "createdAt" : "2023-12-09T01:50:02.188Z"
+                "createdAt" : "2099-12-09T01:50:02.188Z"
               },
-              "updatedAt" : "2023-12-09T01:55:46.988Z",
-              "createdAt" : "2023-12-09T01:55:46.988Z"
+              "updatedAt" : "2099-12-09T01:55:46.988Z",
+              "createdAt" : "2099-12-09T01:55:46.988Z"
             }
         ]
     };
