2ffe494e7b72db8d5c35d23d9309e05abfc5df67
mo <48237066+KingBoyAndGirl@users.noreply.github.com>
2024-08-13 10:48:57 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,1 +1,11 @@
+#!name=B612咔叽
 
+#!desc=B612咔叽会员解锁
+
+[Script]
+
+B612咔叽=type=http-response,pattern=^https?:\/\/user-kaji-api.b612kaji.com\/v1\/(purchase\/subscription\/subscriber\/status|purchase\/item\/user\/info),requires-body=1,script-path=https://raw.githubusercontent.com/WeiGiegie/666/main/b612.js
+
+[MITM]
+
+hostname = %APPEND% purchase-qingtu-api.b612kaji.com,api-qingtu.kajicam.com,user-kaji-api.b612kaji.com
